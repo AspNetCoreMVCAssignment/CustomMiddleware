085ac3bdf8db72cb4e9ca2aeea84148099d6d000
Nikunj018 <delvadiyanikunj1234@gmail.com>
2022-02-21 08:11:51 +0530

    Add project files.

---
+++ CustomMiddleware/Middleware/QueryStringMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CustomMiddleware.Middleware
+{
+    public class QueryStringMiddleware
+    {
+        private RequestDelegate next;
+
+        public QueryStringMiddleware(RequestDelegate _next)
+        {
+            next = _next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            if(context.Request.Method == HttpMethods.Get && context.Request.Query["fname"] == "nikunj")
+            {
+                await context.Response.WriteAsync("Message custom middleware");
+            }
+            await next(context);
+        } 
+    }
+}
